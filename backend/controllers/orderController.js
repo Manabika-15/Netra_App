3f888a6c4a10f56b7988e8a7ea3b8a7eac006ba6
Manabika-15 <dasmanabika.06@gmail.com>
2026-07-08 18:24:13 +0530

    Updated project

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,49 @@
+const Order = require("../model/order")
+const sendEmail = require("../utils/sendEmail")
+
+const createOrder = async (req, res) => {
+    try {
+        const {items, totalAmount, address, paymentId} = req.body;
+        if(!items || items.length === 0 || !totalAmount || !address) {
+            return res.status(400).json({message: "Invalid order data!"})
+        } else {
+            const order = new Order ({
+                user = req.user._id,
+                items,
+                totalAmount,
+                address,
+                paymentId
+            })
+            await order.save();
+            const message = `Dear ${req.user.name}, \n\nThank you for your order!
+            Your Order has been successfully created with the following details: 
+            Order ID: ${order._id}
+            Total Amount: ${totalAmount}
+            Shipping Address: ${address}
+            We will notify you once your order is shipped.
+            Best regards,
+            Netra Team`
+
+            await sendEmail(req.user.email, "Order created!", message)
+            res.status(201).json({message: "Order created successfully", order})
+        }
+    } catch (error) {
+        res.status(500).json({message: "Error creating order", error})
+    }
+}
+
+const getOrders = async (req, res) => {
+    try {
+        const orders = await Order.find({user: req.user._id}).populate('items.productId', 'name price')
+        res.json(orders)
+    } catch (error) {
+        res.status(500).json({message: "Error fetching orders", order})
+    }
+}
+
+
+
+module.exports = {
+    createOrder,
+    getOrders,
+}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,13 +2,17 @@ const express = require("express")
 const router = express.Router()
 const {protect} = require("../middleware/authMiddleware")
 const {admin} = require("../middleware/adminMiddleware")
+const {getProducts, getProductById, createProduct, updateProduct, deleteProduct} = require("../controllers/productController")
+const multer = require("multer")
+const upload = multer({dest: 'upload/'})
+
 
 router.route('/')
 .get(getProducts)
-.post(protect, admin, createProduct)
+.post(protect, admin, upload.single('image'), createProduct)
 router.route('/:id')
 .get(getProductById)
-.put(protect, admin, updateProduct)
+.put(protect, admin, upload.single('image'), updateProduct)
 .delete(protect, admin, deleteProduct)
 
 module.exports = router
