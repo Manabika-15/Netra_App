aeb287e805fdf9c0f31578f96e00835356dd0410
Manabika-15 <dasmanabika.06@gmail.com>
2026-07-23 20:57:36 +0530

    finally

--- backend/index.js
+++ backend/index.js
@@ -23,10 +23,6 @@ app.use(cors(
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get("/" ,(req, res) => {
-    res.send("Netra working!")
-})
-
 app.use('/api/user', userRoute)
 app.use('/api/products', productRoute)
 app.use('/api/orders', orderRoute)
